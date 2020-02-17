cc0ca79165b7e4ed8d32ffde406fbaaee6c571c0
Fujio Nakatsuka <com.nifty.cbm01799>
2020-02-17 19:33:18 +0900

    修正しました

--- Product1/NViewController.swift
+++ Product1/NViewController.swift
@@ -15,7 +15,10 @@ class NiewController: UIViewController {
     var result2 = String()
     var result3 = String()
     var result4 = String()
-    let label = UILabel()
+    let label1 = UILabel()
+    let label2 = UILabel()
+    let label3 = UILabel()
+    let label4 = UILabel()
     
     //中塚さん・・・変数名result1 result2 result3 result4を作成し、その値が渡ってくるので、その値をラベルを4つつけてそれぞれ表示してください。戻るボタンも作成し、ボタンをタップすると前の画面に戻るようにしてください。
     
@@ -24,22 +27,22 @@ class NiewController: UIViewController {
         super.viewDidLoad()
 
      
-        label.frame = CGRect(x:150, y:200, width:160, height:30)
-        label.text = "result1"
-        self.view.addSubview(label)
+        label1.frame = CGRect(x:150, y:250, width:160, height:30)
+        label1.text = "result1"
+        self.view.addSubview(label1)
         
-        label.frame = CGRect(x:150, y:200, width:160, height:30)
-        label.text = "result2"
-               self.view.addSubview(label)
+        label2.frame = CGRect(x:150, y:300, width:160, height:30)
+        label2.text = "result2"
+               self.view.addSubview(label2)
         
-        label.frame = CGRect(x:150, y:200, width:160, height:30)
-        label.text = "result3"
-        self.view.addSubview(label)
+        label3.frame = CGRect(x:150, y:350, width:160, height:30)
+        label3.text = "result3"
+        self.view.addSubview(label3)
         
         
-        label.frame = CGRect(x:150, y:200, width:160, height:30)
-        label.text = "result4"
-        self.view.addSubview(label)
+        label4.frame = CGRect(x:150, y:400, width:160, height:30)
+        label4.text = "result4"
+        self.view.addSubview(label4)
         
         
     }
