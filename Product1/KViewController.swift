d0e20ca466e5d20f138159522c8d5a7559311af3
Yuta Fujii <yutayutakid21@gmail.com>
2020-02-17 18:16:05 +0900

    課題

---
+++ Product1/KViewController.swift
@@ -0,0 +1,33 @@
+//
+//  KViewController.swift
+//  Product1
+//
+//  Created by Fujii Yuta on 2020/02/17.
+//  Copyright © 2020 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class KViewController: UIViewController {
+
+    //近藤さん・・・textFieldを2つ作成し、下にラベルを4つ作成し、ボタンを配置。calcModelクラスを新規に作成し、計算モデルを作成してください。
+    //textFieldで入力された値を上から入力フォーム1、入力フォーム2、足し算、引き算、割り算、掛け算の結果をボタンが押されたら表示し、2秒後に値を4つNViewControllerへsegueを用いて渡してください。idはnextで大丈夫です。遷移はmodalです。変数名はresult1 result2 result3 result4で大丈夫です。
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Product1/NViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Product1
+//
+//  Created by Fujii Yuta on 2020/02/17.
+//  Copyright © 2020 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class NiewController: UIViewController {
+
+    
+    //中塚さん・・・変数名result1 result2 result3 result4を作成し、その値が渡ってくるので、その値をラベルを4つつけてそれぞれ表示してください。戻るボタンも作成し、ボタンをタップすると前の画面に戻るようにしてください。
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
