432c501975ce659eff0b2fcbbc7d2c64c83436b5
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-17 19:01:01 +0900

    textField1を実装

--- Product1/KViewController.swift
+++ Product1/KViewController.swift
@@ -10,24 +10,44 @@ import UIKit
 
 class KViewController: UIViewController {
 
+   
+    
     //近藤さん・・・textFieldを2つ作成し、下にラベルを4つ作成し、ボタンを配置。calcModelクラスを新規に作成し、計算モデルを作成してください。
     //textFieldで入力された値を上から入力フォーム1、入力フォーム2、足し算、引き算、割り算、掛け算の結果をボタンが押されたら表示し、2秒後に値を4つNViewControllerへsegueを用いて渡してください。idはnextで大丈夫です。遷移はmodalです。変数名はresult1 result2 result3 result4で大丈夫です。
+    
+    //入力するテキストフィールド
+    var textField1 = UITextField()
+    var textField2 = UITextField()
+    
+    //足し算
+    var result1 = UILabel()
+    //引き算
+    var result2 = UILabel()
+    //割り算
+    var result3 = UILabel()
+    //掛け算
+    var result4 = UILabel()
+    
+    var padding:CGFloat = 50
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureTextField() {
+        view.addSubview(textField1)
+        textField1.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            textField1.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+                textField1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                textField1.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+                textField1.heightAnchor.constraint(equalToConstant: 50)
+            ])
+        
     }
-    */
+   
 
 }
