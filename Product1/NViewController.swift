ca14e29538af56d8bf146f302fd828ede31bbb5b
Fujio Nakatsuka <com.nifty.cbm01799>
2020-02-17 19:16:03 +0900

    var は入れましたがLabelが。。。。

--- Product1/NViewController.swift
+++ Product1/NViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
+var result1 = String()
+var result2 = String()
+var result3 = String()
+var result4 = String()
+
 class NiewController: UIViewController {
+
 
     
     //中塚さん・・・変数名result1 result2 result3 result4を作成し、その値が渡ってくるので、その値をラベルを4つつけてそれぞれ表示してください。戻るボタンも作成し、ボタンをタップすると前の画面に戻るようにしてください。
