8585bc999a1a39ddd95b7763a548cd5e344ffc95
Fujio Nakatsuka <com.nifty.cbm01799>
2020-02-17 19:29:22 +0900

    とりあえずアラートは出ていませんが。。。。？？？？

--- Product1/NViewController.swift
+++ Product1/NViewController.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-var result1 = String()
-var result2 = String()
-var result3 = String()
-var result4 = String()
 
 class NiewController: UIViewController {
 
-
+    var result1 = String()
+    var result2 = String()
+    var result3 = String()
+    var result4 = String()
+    let label = UILabel()
     
     //中塚さん・・・変数名result1 result2 result3 result4を作成し、その値が渡ってくるので、その値をラベルを4つつけてそれぞれ表示してください。戻るボタンも作成し、ボタンをタップすると前の画面に戻るようにしてください。
     
@@ -23,6 +23,25 @@ class NiewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+     
+        label.frame = CGRect(x:150, y:200, width:160, height:30)
+        label.text = "result1"
+        self.view.addSubview(label)
+        
+        label.frame = CGRect(x:150, y:200, width:160, height:30)
+        label.text = "result2"
+               self.view.addSubview(label)
+        
+        label.frame = CGRect(x:150, y:200, width:160, height:30)
+        label.text = "result3"
+        self.view.addSubview(label)
+        
+        
+        label.frame = CGRect(x:150, y:200, width:160, height:30)
+        label.text = "result4"
+        self.view.addSubview(label)
+        
+        
     }
 
 
