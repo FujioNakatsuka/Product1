efc8f5458714e0aa3cf945b0c64f6e89d3f03659
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-17 19:31:23 +0900

    CalcModelを実装

---
+++ Product1/calcModel.swift
@@ -0,0 +1,37 @@
+//
+//  calcModel.swift
+//  Product1
+//
+//  Created by 近藤宏輝 on 2020/02/17.
+//  Copyright © 2020 Fujii Yuta. All rights reserved.
+//
+
+import Foundation
+
+class CalcModel {
+    var number1 = Int()
+    var number2 = Int()
+    
+    init(CalcA: Int,CalcB: Int) {
+        number1 = CalcA
+        number2 = CalcB
+    }
+    
+    func plus() -> Int {
+        return number1 + number2
+    }
+    
+    func minus() -> Int {
+        return number1 - number2
+    }
+
+    func div() -> Int {
+        return number1 / number2
+    }
+    
+    func mul() -> Int {
+        return number1 * number2
+    }
+
+    
+}
